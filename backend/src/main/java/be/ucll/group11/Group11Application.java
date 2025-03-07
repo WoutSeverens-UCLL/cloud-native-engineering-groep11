a447fd024690013086613a74d9de1ca9affeb4cf
WoutSeverens-UCLL <r0933353@ucll.be>
2025-03-07 15:05:10 +0100

    initial setup

---
+++ backend/src/main/java/be/ucll/group11/Group11Application.java
@@ -0,0 +1,13 @@
+package be.ucll.group11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group11Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/be/ucll/group11/Group11ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.group11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
