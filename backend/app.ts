18e6c224e269a0293a1c2abbe9d4858a75b35738
WoutSeverens-UCLL <r0933353@ucll.be>
2025-04-24 13:37:46 +0200

    added cors and dotenv

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.APP_PORT || 3000;
 
-app.get('/status', (req, res) => {
-    res.json({ message: 'Back-end is running...' });
+app.get("/status", (req, res) => {
+  res.json({ message: "Back-end is running..." });
 });
 
 app.listen(port || 3000, () => {
-    console.log(`Back-end is running on port ${port}.`);
+  console.log(`Back-end is running on port ${port}.`);
 });
-
