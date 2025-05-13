1cf1a63e58ca1e6a654152144b6e70022b953094
WoutSeverens-UCLL <r0933353@ucll.be>
2025-05-13 23:55:44 +0200

    product page first version

---
+++ frontend/pages/products/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@components/header";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Products</h1>
+      <p>List of products will be displayed here.</p>
+    </div>
+  );
+};
+
+export default ProductsPage;
