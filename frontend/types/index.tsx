e9d58ea9309ecc44e09fbf872a1f140f4b9d0237
WoutSeverens-UCLL <r0933353@ucll.be>
2025-05-14 16:06:25 +0200

    made the values of the types optional

--- frontend/types/index.tsx
+++ frontend/types/index.tsx
@@ -1,33 +1,38 @@
 export type User = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  role: string;
+  id?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+  role?: string;
 };
 
 export type Product = {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  brand: string;
-  images: string[];
-  rating: number;
-  colors: string[];
-  sizes: string[];
-  category: string;
-  stock: number;
-  features: string[];
-  reviews: Review[];
-  sellerId: string;
+  id?: string;
+  name?: string;
+  description?: string;
+  price?: number;
+  brand?: string;
+  images?: string[];
+  rating?: number;
+  colors?: string[];
+  sizes?: string[];
+  category?: string;
+  stock?: number;
+  features?: string[];
+  reviews?: Review[];
+  sellerId?: string;
 };
 
 export type Review = {
-  id: string;
-  productId: string;
-  userId: string;
-  rating: number;
-  comment: string;
+  id?: string;
+  productId?: string;
+  userId?: string;
+  rating?: number;
+  comment?: string;
+};
+
+export type StatusMessage = {
+  type: "success" | "error";
+  message: string;
 };
