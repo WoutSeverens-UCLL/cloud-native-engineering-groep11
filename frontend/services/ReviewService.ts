99adaa3b5644ff16745664954585f36032edeaaf
WoutSeverens-UCLL <r0933353@ucll.be>
2025-05-15 16:51:18 +0200

    ReviewService created

---
+++ frontend/services/ReviewService.ts
@@ -0,0 +1,33 @@
+import { Review } from "types";
+
+const getToken = (): string => {
+  const loggedInUserString = sessionStorage.getItem("loggedInUser");
+  return loggedInUserString ? JSON.parse(loggedInUserString).token : "";
+};
+
+const createReview = (review: Review) => {
+  return fetch(process.env.NEXT_PUBLIC_API_URL + "/reviews", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+    body: JSON.stringify(review),
+  });
+};
+
+const getReviewsByProductId = (productId: string) => {
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/reviews/${productId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+};
+
+const ReviewService = {
+  createReview,
+  getReviewsByProductId,
+};
+
+export default ReviewService;
