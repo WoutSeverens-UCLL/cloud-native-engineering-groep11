30e2d95ca35798cd1277b3287f7e89df5b75c93b
Meast04 <leenen.maxime@gmail.com>
2025-05-20 22:20:31 +0200

    get all made

---
+++ serverless-backend/GetProductsHttp/index.ts
@@ -0,0 +1,18 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { ProductService } from "../service/product.service"
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const products = await ProductService.getInstance().getAllProducts();
+
+  context.res = {
+    body: products,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+};
+
+export default httpTrigger;
---
+++ serverless-backend/GetReviewsHttp/index.ts
@@ -0,0 +1,18 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { ReviewService } from "../service/review.service";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const reviews = await ReviewService.getInstance().getAllReviews();
+
+  context.res = {
+    body: reviews,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+};
+
+export default httpTrigger;
