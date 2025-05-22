bf79c4972e472e06adc52ba7536fa760d63748d9
kimberly.austria <kimberly.austria@student.ucll.be>
2025-05-22 18:57:31 +0200

    update product error handling, nog wel met database error product not found

--- serverless-backend/UpdateProductHttp/index.ts
+++ serverless-backend/UpdateProductHttp/index.ts
@@ -1,20 +1,30 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { ProductService } from "../service/product.service"
-
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { ProductService } from "../service/product.service";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  const id = req.params.id;
-  const sellerId = req.params.sellerId;
-  const product = await ProductService.getInstance().updateProduct(id, req.body);
+  try {
+    const id = req.params.id;
 
-  context.res = {
-    body: product,
-    headers: {
-      "Content-Type": "application/json"
-    }
+    const product = await ProductService.getInstance().updateProduct(
+      id,
+      req.body
+    );
+
+    context.res = {
+      body: product,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error: any) {
+    context.res = {
+      status: 400,
+      body: { error: error.message || "Invalid request" },
+      headers: { "Content-Type": "application/json" },
+    };
   }
 };
 
