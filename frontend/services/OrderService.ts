fe831eb6033a7eb85f40d3512b357583f6373b6c
WoutSeverens-UCLL <r0933353@ucll.be>
2025-05-22 15:25:08 +0200

    orderService file

---
+++ frontend/services/OrderService.ts
@@ -0,0 +1,91 @@
+import { Order, OrderStatus } from "types";
+
+const getToken = (): string => {
+  const loggedInUserString = sessionStorage.getItem("loggedInUser");
+  return loggedInUserString ? JSON.parse(loggedInUserString).token : "";
+};
+
+const createOrder = async (order: Order) => {
+  return fetch(process.env.NEXT_PUBLIC_API_URL + "/orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+    body: JSON.stringify(order),
+  });
+};
+
+const getOrder = (id: string, buyerId: string) => {
+  return fetch(process.env.NEXT_PUBLIC_API_URL + `/orders/${id}/${buyerId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+};
+
+const updateOrderStatus = (
+  id: string,
+  buyerId: string,
+  status: OrderStatus
+) => {
+  return fetch(process.env.NEXT_PUBLIC_API_URL + `/orders/${id}/${buyerId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+    body: JSON.stringify({ status }),
+  });
+};
+
+// Get orders for a user (buyer or seller)
+const getOrdersByUser = (userId: string, role: "buyer" | "seller") => {
+  return fetch(
+    process.env.NEXT_PUBLIC_API_URL + `/orders/user/${role}/${userId}`,
+    {
+      method: "GET",
+
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getToken()}`,
+      },
+    }
+  );
+};
+
+const getOrdersByProduct = (productId: string) => {
+  return fetch(
+    process.env.NEXT_PUBLIC_API_URL + `/orders/product/${productId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getToken()}`,
+      },
+    }
+  );
+};
+
+const deleteOrder = (id: string, buyerId: string) => {
+  return fetch(process.env.NEXT_PUBLIC_API_URL + `/orders/${id}/${buyerId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+};
+
+const OrderService = {
+  createOrder,
+  getOrder,
+  updateOrderStatus,
+  getOrdersByUser,
+  getOrdersByProduct,
+  deleteOrder,
+};
+
+export default OrderService;
