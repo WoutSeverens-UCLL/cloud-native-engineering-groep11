0086668f21b7d60ea7c3f653b4e9a7551869c161
kimberlyaustria <kimberly.austria@student.ucll.be>
2025-05-25 17:12:56 +0200

    working users login, create and page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_API_URL: "http://localhost:3000",
+    NEXT_PUBLIC_API_URL: "https://cne-azfunction.azurewebsites.net/api",
+    FK_USERS_LOGIN:
+      "?code=RFxu2eoYU3gYJuwHon-4LwJw3aSmAXJE0ziX6ueZYXqzAzFuebmKrQ==",
+    FK_USERS: "?code=jaHufhTCxLAr2cWhXLw0Mpy7qAk42H2AXG_286XNvWoUAzFuE1ayVA==",
+    FK_USERS_SIGNUP:
+      "?code=w2Ihl-6mr7N3FzEyQVwXkFaJVrkgAUVfvF7N4soa71pCAzFuctak3A==",
   },
   output: "export",
 };
